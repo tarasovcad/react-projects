605f7f7894242923ff586944c12d89ad1aa10359
Maksym <tarasovcad@gmail.com>
2024-04-27 15:35:05 -0500

    feat: add 'The cart is empty'

--- react-sneakers/src/components/Drawer.js
+++ react-sneakers/src/components/Drawer.js
@@ -15,56 +15,76 @@ export function Drawer({ onClose, onRemove, items = [] }) {
             />
           </button>
         </div>
-        <div className="card__items">
-          {items.map((obj) => (
-            <div className="card__item">
-              <img
-                className="card__item-img"
-                src={obj.imageUrl}
-                alt=""
-                width="70px"
-                height="70px"
-              />
-              <div className="card__item-info">
-                <h5 className="card__item-title">{obj.title}</h5>
-                <span className="price">{obj.price} $</span>
-              </div>
-              <button className="card__item-button" onClick={() => onRemove(obj.id)}>
+        {items.length > 0 ? (
+          <div>
+            <div className="card__items">
+              {items.map((obj) => (
+                <div className="card__item">
+                  <img
+                    className="card__item-img"
+                    src={obj.imageUrl}
+                    alt=""
+                    width="70px"
+                    height="70px"
+                  />
+                  <div className="card__item-info">
+                    <h5 className="card__item-title">{obj.title}</h5>
+                    <span className="price">{obj.price} $</span>
+                  </div>
+                  <button className="card__item-button" onClick={() => onRemove(obj.id)}>
+                    <img
+                      className="card__item-close"
+                      src="/img/plus.svg"
+                      alt="remove"
+                      width={11}
+                      height={11}
+                    />
+                  </button>
+                </div>
+              ))}
+            </div>
+            <div className="cart-total">
+              <ul className="cart-total__list">
+                <li className="cart-total__link">
+                  <p className="cart-total__title">Total: </p>
+                  <span className="dash"></span>
+                  <span className="cart-total__price">21 498 $</span>
+                </li>
+                <li className="cart-total__link">
+                  <p className="cart-total__title">Tax 5%: </p>
+                  <span className="dash"></span>
+                  <span className="cart-total__price">1074 $</span>
+                </li>
+              </ul>
+              <button className="cart-total__buton">
+                <span className="cart-total__button-text">Order</span>
                 <img
-                  className="card__item-close"
-                  src="/img/plus.svg"
-                  alt="remove"
-                  width={11}
-                  height={11}
+                  className="cart-total__button-img"
+                  src="/img/order-img.svg"
+                  alt="order svg"
+                  width="16"
+                  heigh="14"
                 />
               </button>
             </div>
-          ))}
-        </div>
-        <div className="cart-total">
-          <ul className="cart-total__list">
-            <li className="cart-total__link">
-              <p className="cart-total__title">Total: </p>
-              <span className="dash"></span>
-              <span className="cart-total__price">21 498 $</span>
-            </li>
-            <li className="cart-total__link">
-              <p className="cart-total__title">Tax 5%: </p>
-              <span className="dash"></span>
-              <span className="cart-total__price">1074 $</span>
-            </li>
-          </ul>
-          <button className="cart-total__buton">
-            <span className="cart-total__button-text">Order</span>
-            <img
-              className="cart-total__button-img"
-              src="/img/order-img.svg"
-              alt="order svg"
-              width="16"
-              heigh="14"
-            />
-          </button>
-        </div>
+          </div>
+        ) : (
+          <div className="cart__empty">
+            <img className="cart__empty-img" src="/img/empty.png" alt="empty" />
+            <h2 className="cart__empty-title">The cart is empty</h2>
+            <p className="cart__empty-text">Add at least one pair of sneakers to place an order.</p>
+            <button className="cart__empty-button">
+              <img
+                className="cart__empty-buttonImg"
+                src="/img/arrow.svg"
+                alt="arrow"
+                width={14}
+                height={12}
+              />
+              Go back
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
