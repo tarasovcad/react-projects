0172d34f37a4a7846e221e003d0a2aa9b07fd244
Maksym <tarasovcad@gmail.com>
2024-05-01 20:03:50 -0500

    feat: display items using json

--- react-task-1/src/components/Product/Product.jsx
+++ react-task-1/src/components/Product/Product.jsx
@@ -4,17 +4,17 @@ import './style.scss';
 
 import React from 'react';
 
-export const Product = () => {
+export const Product = ({ product }) => {
+  const { img, title, price, count, id } = product;
+  console.log(product, 'Product');
   return (
     <section className="product">
       <div className="product__img">
-        <img src="./img/products/macbook.jpg" alt="Apple MacBook Air 13" />
+        <img src={`./img/products/${img}`} alt={title} />
       </div>
-      <div className="product__title">Apple MacBook Air 13</div>
-      <div className="product__count">
-        <Count />
-      </div>
-      <div className="product__price">110 000 руб.</div>
+      <div className="product__title">{title}</div>
+      <div className="product__count">{/* <Count /> */}</div>
+      <div className="product__price">{price} руб.</div>
       <div className="product__controls">
         <ButtonDelete />
       </div>
