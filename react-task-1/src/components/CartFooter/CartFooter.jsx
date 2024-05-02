cd9a32c668b35d75563c090ff411b82daef85ade
Maksym <tarasovcad@gmail.com>
2024-05-01 22:02:41 -0500

    feat: add priceFormatter

--- react-task-1/src/components/CartFooter/CartFooter.jsx
+++ react-task-1/src/components/CartFooter/CartFooter.jsx
@@ -1,12 +1,13 @@
 import './style.scss';
 import React from 'react';
+import priceFormatter from './../../utils/priceFormatter';
 
 export const CartFooter = ({ total }) => {
   const { count, price } = total;
   return (
     <footer className="cart-footer">
       <div className="cart-footer__count">{count} items</div>
-      <div className="cart-footer__price">{price} $</div>
+      <div className="cart-footer__price">{priceFormatter.format(price)} $</div>
     </footer>
   );
 };
--- react-task-1/src/components/Product/Product.jsx
+++ react-task-1/src/components/Product/Product.jsx
@@ -1,11 +1,13 @@
 import { ButtonDelete } from '../ButtonDelete/ButtonDelete';
 import { Count } from '../Count/Count';
 import './style.scss';
+import priceFormatter from './../../utils/priceFormatter';
 
 import React from 'react';
 
 export const Product = ({ product, deleteProducts, increase, decrease, changeValue }) => {
   const { img, title, priceTotal, count, id } = product;
+
   return (
     <section className="product">
       <div className="product__img">
@@ -13,9 +15,15 @@ export const Product = ({ product, deleteProducts, increase, decrease, changeVal
       </div>
       <div className="product__title">{title}</div>
       <div className="product__count">
-        <Count count={count} increase={increase} decrease={decrease} id={id} changeValue={changeValue}/>
+        <Count
+          count={count}
+          increase={increase}
+          decrease={decrease}
+          id={id}
+          changeValue={changeValue}
+        />
       </div>
-      <div className="product__price">{priceTotal} $</div>
+      <div className="product__price">{priceFormatter.format(priceTotal)} $</div>
       <div className="product__controls">
         <ButtonDelete deleteProducts={deleteProducts} id={id} />
       </div>
