464ce270751576dc384a917c97e409575a467cfe
Maksym <tarasovcad@gmail.com>
2024-05-05 12:23:21 -0500

    feat: add useNavigate hook

--- react-task-2/src/pages/cart/Cart.jsx
+++ react-task-2/src/pages/cart/Cart.jsx
@@ -3,10 +3,13 @@ import { PRODUCTS } from '../../products';
 import { ShopContext } from '../../context/shop-context';
 import { CartItem } from './Cart-item';
 import './cart.css';
+import { useNavigate } from 'react-router-dom';
 
 export const Cart = () => {
   const { cartItems, getTotalCartAmount } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
   return (
     <div className="cart">
       <div>
@@ -21,7 +24,7 @@ export const Cart = () => {
       </div>
       <div className="checkout">
         <p>Subtotal: ${totalAmount}</p>
-        <button> Continue Shopping</button>
+        <button onClick={() => navigate('/')}> Continue Shopping</button>
         <button> Checkout</button>
       </div>
     </div>
