7b6c977d974b4e4290bb77a192e8d6b1bb6c3137
Maksym <tarasovcad@gmail.com>
2024-05-05 18:13:11 -0500

    feat: display quiz content

--- react-task-4/src/components/Quiz/Quiz.jsx
+++ react-task-4/src/components/Quiz/Quiz.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Quiz.css';
+import { data } from '../../assets/data';
 export const Quiz = () => {
+  //index - the index of the current quiz question, the initial value of which is set to 0.
+  //question - the current quiz question, which is extracted from the data array by the index "index".
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(data[index]);
   return (
     <div className="container">
       <h1>Quiz app</h1>
       <hr />
-      <h2>1. Which manis asdasd?</h2>
+      <h2>
+        {index + 1}. {question.question}
+      </h2>
       <ul>
-        <li>Modern</li>
-        <li>Router</li>
-        <li>an caksd</li>
-        <li>dog</li>
+        <li>{question.option1}</li>
+        <li>{question.option2}</li>
+        <li>{question.option3}</li>
+        <li>{question.option4}</li>
       </ul>
       <button>Next</button>
       <div className="index">1 of 5 questions</div>
