1fa176ec1028c5a60d3c65e59b7d3ef766f82c15
Maksym <tarasovcad@gmail.com>
2024-05-05 11:17:57 -0500

    feat: add and remove items from the CartPage

--- react-task-2/src/pages/cart/Cart-item.jsx
+++ react-task-2/src/pages/cart/Cart-item.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ShopContext } from '../../context/shop-context';
 
 export const CartItem = (props) => {
   const { id, productName, price, productImage } = props.data;
+  const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
   return (
     <div className="cartItem">
       <img src={productImage} alt="" />
@@ -10,6 +12,11 @@ export const CartItem = (props) => {
           <b>{productName}</b>
         </p>
         <p>${price}</p>
+        <div className="countHandler">
+          <button onClick={() => removeFromCart(id)}>-</button>
+          <input value={cartItems[id]} />
+          <button onClick={() => addToCart(id)}>+</button>
+        </div>
       </div>
     </div>
   );
