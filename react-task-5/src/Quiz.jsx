3336a11fe99bd50d85dcd6c186b6189e901f84ea
Maksym <tarasovcad@gmail.com>
2024-05-06 17:47:24 -0500

    feat: display selected-answer you choose

--- react-task-5/src/Quiz.jsx
+++ react-task-5/src/Quiz.jsx
@@ -3,25 +3,32 @@ import React, { useState } from 'react';
 export const Quiz = ({ questions }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [answerIdx, setAnswerIdx] = useState(null);
-  const { question, choices, type, correctAnswer } = questions[currentQuestion];
+  const [answer, setAnswer] = useState(null);
+
+  const { question, choices, correctAnswer } = questions[currentQuestion];
   const onAnswerClick = (answer, index) => {
     setAnswerIdx(index);
     if (answer === correctAnswer) {
+      setAnswer(true);
       setCurrentQuestion(currentQuestion + 1);
-      a;
+    } else {
+      setAnswer(false);
     }
   };
   return (
     <div className="quiz-container">
       <>
         <span className="active-question-no">{currentQuestion + 1}</span>
-        <span className="total-question">/{questions.length}</span >
+        <span className="total-question">/{questions.length}</span>
         <h2>{question}</h2>
         <ul>
           {choices.map((answer, index) => {
             console.log(answer);
             return (
-              <li onClick={() => onAnswerClick(answer, index)} key={answer}>
+              <li
+                className={answerIdx === index ? 'selected-answer' : ''}
+                onClick={() => onAnswerClick(answer, index)}
+                key={answer}>
                 {answer}
               </li>
             );
