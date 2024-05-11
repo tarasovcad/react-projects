91cf2868236223941ec1e83f3b5b057f8a5e5aaf
Maksym <tarasovcad@gmail.com>
2024-05-11 10:46:59 -0500

    feat: get the random symbols

--- react-task-6/src/App.jsx
+++ react-task-6/src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaClipboard } from 'react-icons/fa';
 import { useForm } from './useForm';
+import { getRundomCharacter, getSpecialChar } from './utils';
 export const App = () => {
   const [values, setValues] = useForm({
     //key: value,
@@ -10,13 +11,33 @@ export const App = () => {
     number: true,
     symbol: true,
   });
+  const [result, setResult] = useState('');
+  const fieldsArray = [
+    {
+      field: values.capital,
+      getChar: getRundomCharacter(65, 90),
+    },
+    {
+      field: values.small,
+      getChar: getRundomCharacter(97, 122),
+    },
+    {
+      field: values.number,
+      getChar: getRundomCharacter(48, 57),
+    },
+    {
+      field: values.symbol,
+      getChar: getSpecialChar(),
+    },
+  ];
+  console.log(fieldsArray);
 
   return (
     <section>
       <div className="container">
         <form id="pg-form">
           <div className="result">
-            <input type="text" id="result" placeholder="Min 6 Char" readOnly />
+            <input type="text" id="result" placeholder="Min 6 Char" readOnly value={result} />
             <div className="clipboard">
               <FaClipboard></FaClipboard>
             </div>
@@ -24,23 +45,55 @@ export const App = () => {
           <div>
             <div className="field">
               <label htmlFor="length">Length</label>
-              <input type="number" id="length" min={6} max={10} name="length" value={values.length} onChange={setValues}/>
+              <input
+                type="number"
+                id="length"
+                min={6}
+                max={10}
+                name="length"
+                value={values.length}
+                onChange={setValues}
+              />
             </div>
             <div className="field">
               <label htmlFor="capital">Capital</label>
-              <input type="checkbox" id="capital" />
+              <input
+                type="checkbox"
+                id="capital"
+                name="capital"
+                checked={values.capital}
+                onChange={setValues}
+              />
             </div>
             <div className="field">
               <label htmlFor="small">Small</label>
-              <input type="checkbox" id="small" />
+              <input
+                type="checkbox"
+                id="small"
+                name="small"
+                checked={values.small}
+                onChange={setValues}
+              />
             </div>
             <div className="field">
               <label htmlFor="number">Number</label>
-              <input type="checkbox" id="number" />
+              <input
+                type="checkbox"
+                id="number"
+                name="number"
+                checked={values.number}
+                onChange={setValues}
+              />
             </div>
             <div className="field">
               <label htmlFor="symbol">Symbol</label>
-              <input type="checkbox" id="symbol" />
+              <input
+                type="checkbox"
+                id="symbol"
+                name="symbol"
+                checked={values.symbol}
+                onChange={setValues}
+              />
             </div>
           </div>
           <button type="submit">Generate password</button>
---
+++ react-task-6/src/utils.jsx
@@ -0,0 +1,9 @@
+export function getRundomCharacter(min, max) {
+  const limit = max - min + 1;
+  return String.fromCharCode(Math.floor(Math.random() * limit) + min);
+}
+
+export function getSpecialChar() {
+  const specialChar = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~'";
+  return specialChar[Math.floor(Math.random() * specialChar.length)];
+}
