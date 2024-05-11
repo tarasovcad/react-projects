5badf64e41366d994b9ca163073bd7f11a695ef4
Maksym <tarasovcad@gmail.com>
2024-05-11 14:37:29 -0500

    feat: follow the checkboxed(if they are pressed)

--- react-task-7/src/components/Form.jsx
+++ react-task-7/src/components/Form.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { generatePassword } from '../utils/form';
 export const Form = () => {
-  console.log(generatePassword(true, true, 41));
+  const numberRef = useRef();
+  const symbolsRef = useRef();
+  const lengthRef = useRef();
+  const [password, setPassowrd] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(
+      generatePassword(
+        numberRef.current.checked,
+        symbolsRef.current.checked,
+        lengthRef.current.value || 6,
+      ),
+    );
+  };
+
   return (
-    <form className="password__form">
+    <form className="password__form" onSubmit={handleSubmit}>
       <h2>Generate a secure password</h2>
       <div className="password__inputs">
         <h4 className="password__text">sdfsdfsf</h4>
@@ -15,15 +29,16 @@ export const Form = () => {
             min={6}
             name="password-length"
             style={{ maxWidth: '8ch' }}
+            ref={lengthRef}
           />
         </div>
         <div className="flex">
           <label htmlFor="numbers">Include numbers ?</label>
-          <input type="checkbox" name="numbers" />
+          <input type="checkbox" name="numbers" ref={numberRef} />
         </div>
         <div className="flex">
           <label htmlFor="symbols">Include symbols ?</label>
-          <input type="checkbox" name="symbols" />
+          <input type="checkbox" name="symbols" ref={symbolsRef} />
         </div>
         <button className="btn">generate</button>
       </div>
