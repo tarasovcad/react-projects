12b769e068e3f503c6c02af29eea175a917b7194
Maksym <tarasovcad@gmail.com>
2024-05-16 20:33:03 -0500

    init: start the shoppig cart-v1

--- TypeScript/cart-project-v1/src/context/ProductsProvider.tsx
+++ TypeScript/cart-project-v1/src/context/ProductsProvider.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
-
-export const ProductsProvider = () => {
-  return <div>ProductsProvider</div>;
+export type ProductType = {
+  sku: string;
+  name: string;
+  price: number;
 };
+
+const initState: ProductType[] = [
+  {
+    sku: 'item0001',
+    name: 'Widget',
+    price: 9.99,
+  },
+  {
+    sku: 'item0002',
+    name: 'Premium Widget',
+    price: 19.99, 
+  },
+  {
+    sku: 'item0003',
+    name: 'Deluxe Widget',
+    price: 29.99,
+  },
+];
+
+
+export type UseProductsContextType = {products: ProductType[]}
---
+++ TypeScript/shopping-cart-v1/src/App.tsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Home } from './pages/Home';
+import { Store } from './pages/Store';
+import { About } from './pages/About';
+import { Navbar } from './components/Navbar';
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
