e1e2ec4a3889cdf59273b6fffe9019a55be940a4
Maksym <tarasovcad@gmail.com>
2024-05-16 21:04:10 -0500

    feat: add carts and formatCurrency function

--- TypeScript/shopping-cart-v1/src/pages/Store.tsx
+++ TypeScript/shopping-cart-v1/src/pages/Store.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import storeItems from '../data/items.json';
+import { Col, Row } from 'react-bootstrap';
+import { StoreItem } from '../components/StoreItem';
 
 export const Store = () => {
-  return <div>Store</div>;
+  return (
+    <>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+        <Col></Col>
+      </Row>
+    </>
+  );
 };
