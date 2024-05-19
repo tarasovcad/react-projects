00610018f728250c4e7bd6362e195a06176edf62
Maksym <tarasovcad@gmail.com>
2024-05-19 00:12:23 -0500

    feat: add function to delete the transaction

--- ExpenseTracker/expense-tracker/src/context/AppReducer.jsx
+++ ExpenseTracker/expense-tracker/src/context/AppReducer.jsx
@@ -1,6 +1,18 @@
 export default (state, action) => {
-    switch(action.type) {
-        default:
-            return state;
-    }
+  // The function takes two parameters:
+  // state: The current state of the application.
+  // action: An object that describes what happened and how the state should change.
+  switch (action.type) {
+    // switch(action.type): This checks the type of action that was dispatched. Based on the action type, different logic will be executed.
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state, // copy all properties from the current state to the new state.
+        transactions: state.transactions.filter(
+          (transactions) => transactions.id !== action.payload,
+        ),
+      };
+    // default: If the action type doesn't match any cases, the default case returns the current state unchanged.
+    default:
+      return state;
+  }
 };
--- ExpenseTracker/expense-tracker/src/context/GlobalState.jsx
+++ ExpenseTracker/expense-tracker/src/context/GlobalState.jsx
@@ -9,15 +9,23 @@ const InitialState = {
     { id: 4, text: 'Camera', amount: 150 },
   ],
 };
-// Create context
+// A function to create a context, which allows you to share state across multiple components without passing props manually.
 export const GlobalContext = createContext(InitialState);
 
-// Provider component
+// GlobalProvider - A component that will wrap around other components to provide them access to the global state.
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, InitialState);
+  // Actions
+  function deleteTransaction(id) {
+    // The dispatch function sends an action object to the reducer to update the state.
+    dispatch({ type: 'DELETE_TRANSACTION', payload: id });
+  }
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    // This component makes the state and actions available to any child components that need them.
+    <GlobalContext.Provider value={{ transactions: state.transactions, deleteTransaction }}>
       {children}
     </GlobalContext.Provider>
+    // The value prop of the provider is an object containing the current state (transactions) and any actions (like deleteTransaction) you want to provide to the children components.
   );
 };
