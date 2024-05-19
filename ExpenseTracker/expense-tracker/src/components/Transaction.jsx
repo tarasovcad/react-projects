aeba13845176c08f20c0f94816b7f7c4280f13b3
Maksym <tarasovcad@gmail.com>
2024-05-18 23:29:06 -0500

    feat: add Balance and IncomeExpenses

--- ExpenseTracker/expense-tracker/src/components/Transaction.jsx
+++ ExpenseTracker/expense-tracker/src/components/Transaction.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? '-' : '+';
   return (
-    <li className={transaction.amount < 0 ? 'plus' : 'minus'}>
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
       {transaction.text}
       <span>
         {sign}${Math.abs(transaction.amount)}
