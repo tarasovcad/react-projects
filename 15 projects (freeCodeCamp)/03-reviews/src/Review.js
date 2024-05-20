5986e0231994feceff65c61df95e0acde78a2f62
Maksym <tarasovcad@gmail.com>
2024-05-19 19:47:15 -0500

    feat: add right and left button with infinite number for 03-reviews

---
+++ 15 projects (freeCodeCamp)/03-reviews/src/Review.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { id, name, job, text, image } = people[index];
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      console.log(newIndex);
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      console.log(newIndex);
+      return checkNumber(newIndex);
+    });
+  };
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={text} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">suprise me</button>
+    </article>
+  );
+};
+
+export default Review;
