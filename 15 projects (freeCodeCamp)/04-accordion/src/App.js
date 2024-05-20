99ffb0d950c1b4afbf3a9533fa33e140f7499f38
Maksym <tarasovcad@gmail.com>
2024-05-19 21:45:50 -0500

    feat: add 04-according

---
+++ 15 projects (freeCodeCamp)/04-accordion/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+import Question from './Question';
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and</h3>
+        <section className="info">
+          {questions.map((question) => (
+            <SingleQuestion key={question.id} {...question} />
+          ))}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
