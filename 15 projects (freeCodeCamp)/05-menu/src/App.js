127a8f37441b8ca9dc408b30631dbcf92a7e3681
Maksym <tarasovcad@gmail.com>
2024-05-19 22:06:27 -0500

    feat: add category for 05-menu

---
+++ 15 projects (freeCodeCamp)/05-menu/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterItems}/>
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ 15 projects (freeCodeCamp)/05-menu/src/Categories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Categories = ({ filterItems }) => {
+  return (
+    <div className="btn-container">
+      <button
+        className="filter-btn"
+        onClick={() => {
+          filterItems('breakfast');
+        }}>
+        breakfast
+      </button>
+    </div>
+  );
+};
+
+export default Categories;
