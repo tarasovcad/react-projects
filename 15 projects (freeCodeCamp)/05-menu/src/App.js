b6989c1e1729e39a454ea4726e104d83412a0b80
Maksym <tarasovcad@gmail.com>
2024-05-19 22:30:59 -0500

    feat: remove duplicates for 05-menu

--- 15 projects (freeCodeCamp)/05-menu/src/App.js
+++ 15 projects (freeCodeCamp)/05-menu/src/App.js
@@ -3,11 +3,18 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+console.log(allCategories);
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
   const filterItems = (category) => {
-    const newItems = items.filter((item) => item.category === category)
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
     setMenuItems(newItems);
   };
   return (
@@ -17,7 +24,7 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems}/>
+        <Categories filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
