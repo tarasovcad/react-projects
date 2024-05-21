e1475fc48b12c5f3aee3da0155840fc09e339ae4
Maksym <tarasovcad@gmail.com>
2024-05-20 19:28:55 -0500

    feat: get values from input for 09-color-generator

---
+++ 15 projects (freeCodeCamp)/09-color-generator/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+
+import Values from 'values.js';
+
+function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+    } catch (e) {
+      setError(true);
+      console.log(e);
+    }
+  };
+  return (
+    <>
+      <section className="container">
+        <h1>color generator project</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            placeholder="#f15025"
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+            className={`${error? 'error' : null}`}
+          />
+          <button type="submit" className="btn">
+            submit
+          </button>
+        </form>
+        {/* <SingleColor />
+        <Values /> */}
+      </section>
+      <section className="colors">
+        <h4>List goes here</h4>
+      </section>
+      ;
+    </>
+  );
+}
+
+export default App;
