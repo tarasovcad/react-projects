f538d7a31a3625f03f89945e9a696235e684feb3
Maksym <tarasovcad@gmail.com>
2024-05-20 21:35:49 -0500

    feat: add initial value for 09-color-generator

--- 15 projects (freeCodeCamp)/09-color-generator/src/App.js
+++ 15 projects (freeCodeCamp)/09-color-generator/src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js';
 function App() {
   const [color, setColor] = useState('#231');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#231').all(10));
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
