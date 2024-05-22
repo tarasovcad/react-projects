918809ac23106199f1b0eb43d21090d8dce45461
Maksym <tarasovcad@gmail.com>
2024-05-22 18:46:28 -0500

    feat: add smooth navbar opening on burger click for 11-navbar

--- 15 projects (freeCodeCamp)/11-navbar/src/Navbar.js
+++ 15 projects (freeCodeCamp)/11-navbar/src/Navbar.js
@@ -4,16 +4,17 @@ import { links, social } from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
+        <div className={`${showLinks ? "links-container show-container" : "links-container"}`}>
           <ul className="links">
             {links.map((link) => {
               const { id, url, text } = link;
