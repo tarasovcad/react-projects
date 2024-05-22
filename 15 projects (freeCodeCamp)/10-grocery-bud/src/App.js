c9af6d7b2f57f01a23389a445edf01381c3ded30
Maksym <tarasovcad@gmail.com>
2024-05-21 22:01:52 -0500

    feat: add localStorage for 10-grocery-bud

--- 15 projects (freeCodeCamp)/10-grocery-bud/src/App.js
+++ 15 projects (freeCodeCamp)/10-grocery-bud/src/App.js
@@ -2,9 +2,19 @@ import React, { useState, useEffect } from 'react';
 import List from './List';
 import Alert from './Alert';
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return JSON.parse(localStorage.getItem('list'));
+  }
+  else {
+    return []
+  }
+}
+
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
@@ -51,6 +61,9 @@ function App() {
     setEditID(id);
     setName(specificItem.title);
   };
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
   return (
     <section className="section-center">
       {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
