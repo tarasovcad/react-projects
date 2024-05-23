7645fb43dd3479354eb41b6154da65ff1abc4780
Maksym <tarasovcad@gmail.com>
2024-05-22 20:37:50 -0500

    feat: add sidebar structure for 12-sidebar-modal

---
+++ 15 projects (freeCodeCamp)/12-sidebar-modal/src/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+const Modal = () => {
+  return (
+    <div className={`modal-overlay show-modal`}>
+      <div className="modal-container">
+        <h4>modal container</h4>
+        <button className="close-modal-btn">
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
