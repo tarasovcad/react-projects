1e41617d8145e047b3f85be01779789d52598e5f
Maksym <tarasovcad@gmail.com>
2024-05-23 19:29:37 -0500

    feat: add Global Context for 13-stripe-submenu

---
+++ 15 projects (freeCodeCamp)/13-stripe-submenus/src/context.js
@@ -0,0 +1,41 @@
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+      }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+// custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
