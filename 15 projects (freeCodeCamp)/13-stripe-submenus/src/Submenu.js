cdae37cac9d41e0dc8c1557457487c4fe6a8526c
Maksym <tarasovcad@gmail.com>
2024-05-23 21:36:34 -0500

    feat: display links on hover with depending colums for 13-stripe-submenus

--- 15 projects (freeCodeCamp)/13-stripe-submenus/src/Submenu.js
+++ 15 projects (freeCodeCamp)/13-stripe-submenus/src/Submenu.js
@@ -6,16 +6,40 @@ const Submenu = () => {
     location,
     page: { page, links },
   } = useGlobalContext();
+
   const container = useRef(null);
+
+  const [columns, setColumns] = useState('col-2');
+
   useEffect(() => {
+    setColumns('col-2');
     const submenu = container.current;
     const { center, bottom } = location;
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
-  }, [location]);
+
+    if (links.length === 3) {
+      setColumns('col-3');
+    }
+    if (links.length > 3) {
+      setColumns('col-4');
+    }
+  }, [location, links]);
+
   return (
     <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`} ref={container}>
       <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link, index) => {
+          const { url, icon, label } = link;
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
     </aside>
   );
 };
