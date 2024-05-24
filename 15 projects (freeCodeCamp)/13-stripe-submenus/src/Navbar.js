6af249c76791d46b41f0c70b50845cbd5fd164a7
Maksym <tarasovcad@gmail.com>
2024-05-23 20:45:11 -0500

    feat: add Submenu basic features for 13-stripe-submenu

--- 15 projects (freeCodeCamp)/13-stripe-submenus/src/Navbar.js
+++ 15 projects (freeCodeCamp)/13-stripe-submenus/src/Navbar.js
@@ -5,6 +5,10 @@ import { useGlobalContext } from './context';
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    console.log('hello wolrd');
+    openSubmenu();
+  };
   return (
     <nav className="nav">
       <div className="nav-center">
@@ -16,16 +20,24 @@ const Navbar = () => {
         </div>
         <ul className="nav-links">
           <li>
-            <button className="link-btn">products</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
           </li>
           <li>
-            <button className="link-btn">developers</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
           </li>
           <li>
-            <button className="link-btn">company</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
           </li>
         </ul>
-        <button className="btn signin-btn">Sign In</button>
+        <button className="btn signin-btn" onMouseOver={displaySubmenu}>
+          Sign In
+        </button>
       </div>
     </nav>
   );
--- 15 projects (freeCodeCamp)/13-stripe-submenus/src/Submenu.js
+++ 15 projects (freeCodeCamp)/13-stripe-submenus/src/Submenu.js
@@ -1,7 +1,8 @@
-import React, { useState, useRef, useEffect } from 'react'
-
+import React, { useState, useRef, useEffect } from 'react';
+import { useGlobalContext } from './context';
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const { isSubmenuOpen } = useGlobalContext();
+  return <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}>submenu</aside>;
+};
 
-export default Submenu
+export default Submenu;
