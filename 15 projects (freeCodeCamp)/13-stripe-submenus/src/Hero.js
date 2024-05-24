16cd903634f209a74b7d8ac94c4e735b1b2a3c6d
Maksym <tarasovcad@gmail.com>
2024-05-23 19:52:11 -0500

    feat: add navbar and hero for 13-stripe-submenu

--- 15 projects (freeCodeCamp)/13-stripe-submenus/src/Hero.js
+++ 15 projects (freeCodeCamp)/13-stripe-submenus/src/Hero.js
@@ -2,9 +2,24 @@ import React from 'react';
 import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context';
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Millions of companies of all sizes use Stripe online and in person to accept payments,
+            send payouts, automate financial processes, and grow revenue.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="phone-img" className="phone" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
