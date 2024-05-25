eecc351fe9d44ee8920c3625372cafb1c745a037
Maksym <tarasovcad@gmail.com>
2024-05-25 18:59:56 -0500

    init: start 14-cart

---
+++ 15 projects (freeCodeCamp)/14-cart/src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    img: 'https://images.samsung.com/is/image/samsung/ca-galaxy-s7-g930w8-sm-g930wzdaxac-000000001-front-gold?$56_56_PNG$',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'google pixel ',
+    price: 499.99,
+    img: 'https://www.bell.ca/Styles/wireless/Google_Pixel_8_pro/Google_Pixel_8_Pro_Bay_lrg1.png',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    img: 'https://guide-images.cdn.ifixit.com/igi/RP3RqUqT2jQ3HRZP.standard',
+    amount: 1,
+  },
+];
