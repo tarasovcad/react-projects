1d7225f0ed3252326b10f5f92cc8da36bdf81eb3
Maksym <tarasovcad@gmail.com>
2024-05-25 21:32:24 -0500

    feat: add navbar, about and error pages for 15-cocktails

--- 15 projects (freeCodeCamp)/15-cocktails/src/pages/About.js
+++ 15 projects (freeCodeCamp)/15-cocktails/src/pages/About.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione consequuntur repellendus,
+        animi fugit cupiditate maiores nisi modi alias fugiat! Delectus, possimus praesentium beatae
+        voluptatum quasi aperiam, nesciunt laudantium, at ab perferendis omnis veniam dolor sunt
+        atque hic. Laudantium magnam odit et saepe sapiente possimus aspernatur vero aut, vitae,
+        pariatur non cupiditate sunt, eaque rem in aliquam fugit facere. Iste, maxime! Deleniti
+        saepe atque nisi vel pariatur ex velit? Quibusdam itaque aperiam error? Consequatur
+        mollitia, animi provident optio voluptatem dolorem magni autem dolorum? Incidunt, nihil
+        minus ipsam nulla quam minima quod impedit facilis natus praesentium nobis ducimus fugiat.
+        Suscipit, maiores dignissimos!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
