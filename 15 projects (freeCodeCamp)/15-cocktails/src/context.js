11c14dd8874c3c2577e12ef366dc7dcaf9baeb31
Maksym <tarasovcad@gmail.com>
2024-05-25 22:32:52 -0500

    feat: setup Context for 15-cocktails

--- 15 projects (freeCodeCamp)/15-cocktails/src/context.js
+++ 15 projects (freeCodeCamp)/15-cocktails/src/context.js
@@ -1,15 +1,22 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { useCallback } from 'react'
+import React, { useState, useContext, useEffect } from 'react';
+import { useCallback } from 'react';
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-const AppContext = React.createContext()
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
-// make sure use
-export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('a');
+  const [cocktails, setCocktails] = useState([]);
 
-export { AppContext, AppProvider }
+  return (
+    <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
