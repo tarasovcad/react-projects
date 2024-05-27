e1ec7b713d06fa62882e84ba0cae3a29b30494bb
Maksym <tarasovcad@gmail.com>
2024-05-26 19:20:56 -0500

    feat: fetching data from api in v1-next

--- Next.js/v1/src/app/prediciton/[name]/page.tsx
+++ Next.js/v1/src/app/prediciton/[name]/page.tsx
@@ -1,12 +1,30 @@
+const getPredictedAge = async (name: string) => {
+  const response = await fetch(`https://api.agify.io?name=${name}`);
+  return response.json();
+};
+const getPredictedGender = async (name: string) => {
+  const response = await fetch(`https://api.genderize.io?name=${name}`);
+  return response.json();
+};
+const getPredictedCountry = async (name: string) => {
+  const response = await fetch(`https://api.nationalize.io?name=${name}`);
+  return response.json();
+};
+
 interface Params {
-  params: {name: string}
+  params: { name: string };
 }
 
-export default function Page({ params }: Params)  {
+export default async function Page({ params }: Params) {
+  const ageData = getPredictedAge(params.name);
+  const genderData = getPredictedGender(params.name);
+  const countryData = getPredictedCountry(params.name);
+
+  const [age, gender, country] = await Promise.all([ageData, genderData, countryData]);
   return (
     <div>
       <h1>predicion</h1>
       {params.name}
     </div>
   );
-};
+}
