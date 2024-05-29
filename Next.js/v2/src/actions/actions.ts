c7e1cb69126168ebe24decd56f0b4ab128b54393
Maksym <tarasovcad@gmail.com>
2024-05-28 23:02:30 -0500

    feat: add authentication to protect route using Kinde for v2-next

--- Next.js/v2/src/actions/actions.ts
+++ Next.js/v2/src/actions/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
 export async function createPost(formData: FormData) {
-    const title  = formData.get('title') as string
-    console.log(title)
+  const title = formData.get('title') as string;
+  console.log(title);
 }
--- Next.js/v2/src/components/posts-list.tsx
+++ Next.js/v2/src/components/posts-list.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import React from 'react';
 
 export default async function PostsList() {
-  //await new Promise((resolve) => setTimeout(resolve, 5000));
   const response = await fetch('https://dummyjson.com/posts?limit=10');
   const data = await response.json();
   return (
