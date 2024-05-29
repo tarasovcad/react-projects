5d8ab8893a42a6ee2272568b345f8c6420a73451
Maksym <tarasovcad@gmail.com>
2024-05-28 21:06:33 -0500

    feat: display the fetch data using params for v2-next

---
+++ Next.js/v2/src/app/posts/[id]/page.tsx
@@ -0,0 +1,11 @@
+export default async function Page({params}: {params: {id:string}}) {
+  const response = await fetch(`https://dummyjson.com/posts/${params.id}`);
+  const data = await response.json();
+  const { title, body } = data;
+  return (
+    <main className="px-7 pt-24 text-center">
+      <h1 className="text-5xl font-semibold">{title}</h1>
+      <p className="max-w-[700px] mx-auto">{body}</p>
+    </main>
+  );
+}
--- Next.js/v2/src/app/posts/page.tsx
+++ Next.js/v2/src/app/posts/page.tsx
@@ -1,10 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
 
-export default function page() {
+export default async function Page() {
+  const response = await fetch('https://dummyjson.com/posts?limit=10');
+  const data = await response.json();
+  console.log(data);
+  console.log(data.posts);
   return (
-    <main className="text-center pt-32 px-5">
+    <main className="text-center pt-16 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">All posts</h1>
+      <ul>
+        {data.posts.map((post) => (
+          <Link href={`posts/${post.id}`}>
+            <li key={post.id}>{post.title}</li>
+          </Link>
+        ))}
+      </ul>
     </main>
   );
 }
- 
