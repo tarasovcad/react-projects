471b7d511b90c6f18566de8a666806ca02f80da7
Maksym <tarasovcad@gmail.com>
2024-05-28 22:11:02 -0500

    feat: add form page for v2-next

---
+++ Next.js/v2/src/actions/actions.ts
@@ -0,0 +1,6 @@
+'use server';
+
+export async function createPost(formData: FormData) {
+    const title  = formData.get('title') as string
+    console.log(title)
+}
---
+++ Next.js/v2/src/components/form.tsx
@@ -0,0 +1,26 @@
+import { createPost } from '@/actions/actions';
+import React from 'react';
+
+export default function Form() {
+  return (
+    <form
+      action={createPost} //Server action
+      className="flex flex-col max-w-[400px] mx-auto gap-2 my-10">
+      <input
+        type="text" 
+        name="title" // need for actions
+        placeholder="Title for new post"
+        className="border rounded px-3 h-10"
+        required
+      />
+      <textarea
+        name="body"
+        placeholder="Body content for new post"
+        className="border rounded px-3 py-2"
+        rows={6}
+        required
+      />
+      <button className="h-10 bg-blue-500 px-5 rounded text-white">Submit</button>
+    </form>
+  );
+}
