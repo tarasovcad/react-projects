289b102b4f5d2248953583e7ba3d27bb93150f6a
Maksym <tarasovcad@gmail.com>
2024-05-29 20:05:44 -0500

    feat: add authentication using middleware for v2-next

--- Next.js/v2/src/app/create-post/page.tsx
+++ Next.js/v2/src/app/create-post/page.tsx
@@ -1,13 +1,6 @@
 import Form from '@/components/form';
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { redirect } from 'next/navigation';
-//import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default async function Page() {
-  const { isAuthenticated } = getKindeServerSession();
-  if (!(await isAuthenticated())) {
-    redirect('/api/auth/login?post_login_redirect_url=/create-post');
-  }
   return (
     <main className="text-center pt-16">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">Create post</h1>
