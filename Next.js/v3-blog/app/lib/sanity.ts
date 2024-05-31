ebd089dcd9b66ced573dcaec7319a8355d003bf4
Maksym <tarasovcad@gmail.com>
2024-05-30 20:12:23 -0500

    feat: disaply information from data and style them for v3-blog

--- Next.js/v3-blog/app/lib/sanity.ts
+++ Next.js/v3-blog/app/lib/sanity.ts
@@ -1,9 +1,13 @@
 import { createClient } from 'next-sanity';
-
+import imageUrlBuilder from '@sanity/image-url';
 export const client = createClient({
   apiVersion: '2023-05-03',
   projectId: 'jc294wbl',
   dataset: 'production',
   useCdn: false,
-  
 });
+
+const builder = imageUrlBuilder(client);
+export function urlFor(source: any) {
+    return builder.image(source);
+}
--- Next.js/v3-blog/app/page.tsx
+++ Next.js/v3-blog/app/page.tsx
@@ -1,4 +1,4 @@
-import { client } from './lib/sanity';
+import { client, urlFor } from './lib/sanity';
 import { simpleBlogCard } from './lib/interface';
 import {
   Card,
@@ -9,6 +9,8 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 async function getData() {
   const query = `
   *[_type == 'blog'] | order(_createdAt asc) {
@@ -26,10 +28,27 @@ export default async function Home() {
   const data: simpleBlogCard[] = await getData();
   console.log(data);
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+    <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5 max-w-screen-lg mx-auto">
       {data.map((post, index) => (
         <Card key={index}>
-          <Image src={} />
+          {post.titleImage ? (
+            <Image
+              src={urlFor(post.titleImage).url()}
+              alt="image"
+              width={500}
+              height={500}
+              className="rounded-t-lg h-[200px] object-cover"
+            />
+          ) : null}
+          <CardContent className="mt-5">
+            <h3 className="text-lg line-clamp-2">{post.title}</h3>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-400">
+              {post.smallDescription}
+            </p>
+            <Button asChild className="w-full mt-7">
+              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+            </Button>
+          </CardContent>
         </Card>
       ))}
     </div>
