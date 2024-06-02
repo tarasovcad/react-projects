88836d5090e6ad39bf3d96ff2760942bc572e95f
Maksym <tarasovcad@gmail.com>
2024-06-01 21:28:33 -0500

    sort the files

--- react-task-1/src/components/Title/Title.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import './style.scss'
-export const Title = () => {
-  return <h1 className="title-1">Корзина товаров</h1>;
-};
--- react-task-1/src/utils/priceFormatter.js
+++
@@ -1,3 +0,0 @@
-const priceFormatter = new Intl.NumberFormat();
-
-export default priceFormatter;
