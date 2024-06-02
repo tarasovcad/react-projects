6c5b19ff755ae41d04a2069e27af4ac095130709
Maksym <tarasovcad@gmail.com>
2024-06-01 19:18:15 -0500

    feat: add posts per tag page for v4-blog

--- Next.js/v4-blog/app/(client)/tag/page.tsx
+++ Next.js/v4-blog/app/(client)/tag/page.tsx
@@ -19,7 +19,6 @@ export const revalidate = 60;
 
 export default async function page() {
   const tags: Tag[] = await getAllTags();
-  console.log(tags);
   return (
     <div>
       <Header title="Tags" />
---
+++ Next.js/v4-blog/app/(client)/tags/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/app/components/Header';
+import React from 'react';
+
+export default function page() {
+  return (
+    <div>
+      <Header title="Post per tag" />
+      fsd
+    </div>
+  );
+}
