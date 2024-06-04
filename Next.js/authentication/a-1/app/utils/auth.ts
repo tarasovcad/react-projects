c67dda7c141228006e39ae139eddedcb99de46e3
Maksym <tarasovcad@gmail.com>
2024-06-04 10:45:35 -0500

    feat: add GitHub provider for a-1

---
+++ Next.js/authentication/a-1/app/utils/auth.ts
@@ -0,0 +1,11 @@
+import { NextAuthOptions } from 'next-auth';
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET_ID as string,
+    }),
+  ],
+} satisfies NextAuthOptions;
