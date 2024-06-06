a60e5c317c955510209a76eee82ebb81e20e86a8
Maksym <tarasovcad@gmail.com>
2024-06-05 20:30:02 -0500

    feat: create a user for a-2

--- Next.js/authentication/a-2/actions/register.ts
+++ Next.js/authentication/a-2/actions/register.ts
@@ -1,16 +1,42 @@
 'use server';
 
 import * as z from 'zod';
-
+import bcrypt from 'bcryptjs';
 import { RegisterSchema } from '@/schemas';
 
+import { db } from '@/lib/db';
+
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  console.log(values);
   // This method attempts to validate the values object against the rules defined in LoginSchema.
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: 'Invalid fields!' };
   }
+
+  const { password, name, email } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO: send verification token email
+
+  return { success: 'User created!' };
+
   return {
     success: 'Email sent!',
   };
