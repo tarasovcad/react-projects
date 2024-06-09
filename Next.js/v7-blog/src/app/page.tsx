fed9d25bc6b272a627cefeb800e6712b48859855
Maksym <tarasovcad@gmail.com>
2024-06-09 12:23:28 -0500

    feat: add FormPost component for v-7

---
+++ Next.js/v7-blog/src/app/page.tsx
@@ -0,0 +1,10 @@
+import FormNewPost from '@/components/FormNewPost';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl mx-auto my-5">
+      <FormNewPost />
+    </main>
+  );
+}
---
+++ Next.js/v7-blog/src/components/Header.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="bg-blue-500 p-4">
+      <nav className="flex justify-between items-center max-w-4xl mx-auto">
+        <Link href="/" className="text-white text-2xl font-bold">
+          My blog
+        </Link>
+
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/blogs" className="text-white hover:underline">
+              Blogs
+            </Link>
+          </li>
+          <li>
+            <Link href="/api/auth/login" className="text-white hover:underline">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
