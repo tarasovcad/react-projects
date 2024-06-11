3cf354d759610e7591fa2cfa697254290021bda7
Maksym <tarasovcad@gmail.com>
2024-06-10 22:44:19 -0500

    feat: made a request to the server using the Fetch API to register route.js for e-1

--- Next.js/E-commerce/e-1/src/app/register/page.js
+++ Next.js/E-commerce/e-1/src/app/register/page.js
@@ -1,12 +1,38 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Image from 'next/image';
 export default function RegisterPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  console.log(email, password);
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    fetch('api/register', {
+      // Makes a request to the server using the Fetch API.
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
-      <form className="block max-w-xs mx-auto">
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Register</button>
         <div className="my-4 text-center text-gray-500">or login with provider</div>
         <button className="flex gap-4 justify-center">
