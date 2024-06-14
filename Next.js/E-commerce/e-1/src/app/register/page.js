8c4f9f401d1cb08f4e02d21e3c0ee8d111bd5258
Maksym <tarasovcad@gmail.com>
2024-06-13 23:58:29 -0500

    feat: display text if user created and disable buttons when fetching for e-1

--- Next.js/E-commerce/e-1/src/app/register/page.js
+++ Next.js/E-commerce/e-1/src/app/register/page.js
@@ -2,38 +2,68 @@
 
 import React, { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 export default function RegisterPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  console.log(email, password);
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [useCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
 
-  function handleFormSubmit(e) {
+  async function handleFormSubmit(e) {
     e.preventDefault();
-    fetch('api/register', {
+    setCreatingUser(true);
+    setError(false);
+    setUserCreated(false);
+    const response = fetch('api/register', {
       // Makes a request to the server using the Fetch API.
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
+    if (response.ok) {
+      setUserCreated(true);
+    } else {
+      setError(true);
+    }
+    setCreatingUser(false);
   }
 
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
+      {useCreated && (
+        <div className="my-4 text-center">
+          User Created. <br /> Now you can
+          <Link className="underline" href={'/login'}>
+            Login &raquo;
+          </Link>
+        </div>
+      )}
+      {error && (
+        <div className="my-4 text-center">
+          An error has occured.. <br />
+          Plese try again later
+        </div>
+      )}
       <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
         <input
           type="email"
           placeholder="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={creatingUser}
         />
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={creatingUser}
         />
-        <button type="submit">Register</button>
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
         <div className="my-4 text-center text-gray-500">or login with provider</div>
         <button className="flex gap-4 justify-center">
           <Image src={'/google.png'} alt={''} width={24} height={24} />
