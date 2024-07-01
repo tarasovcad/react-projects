30f341a79bb6fe902bf0f82189888c707e535209
Maksym <tarasovcad@gmail.com>
2024-07-01 00:27:34 -0500

    feat: connect app to mongoodb for r-5

---
+++ Next.js/API Routes/r-5/app/page.js
@@ -0,0 +1,41 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import connectMongoDB from '@/libs/connect';
+import React from 'react';
+
+export default function page() {
+  connectMongoDB();
+  return (
+    <div className="bg-gray-800 min-h-screen flex items-center justify-center p-6">
+      <div className="w-full max-w-md bg-gray-700 rounded-xl shadow-md">
+        <form className="space-y-3 p-4">
+          <label htmlFor="todo" className="block text-sm font-medium text-gray-300">
+            Name of TO-DO
+          </label>
+          <input
+            type="text"
+            name="todo"
+            id="todo"
+            placeholder="Enter a TODO"
+            className="w-full px-3 py-2 bg-gray-600 border border-gray-500 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 mb-2"
+          />
+          <label htmlFor="todo" className="block text-sm font-medium text-gray-300">
+            Name of Description
+          </label>
+          <input
+            type="text"
+            name="todo"
+            id="todo"
+            placeholder="Enter a Description"
+            className="w-full px-3 py-2 bg-gray-600 border border-gray-500 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+          />
+          <button
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+            type="submit">
+            Add TODO
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
