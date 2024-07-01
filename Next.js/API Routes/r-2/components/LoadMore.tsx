a98aef9ae731461ed2b7a73773fb872f4bcf4ce8
Maksym <tarasovcad@gmail.com>
2024-06-30 20:00:36 -0500

    feat: display new fetching data for r-2

--- Next.js/API Routes/r-2/components/LoadMore.tsx
+++ Next.js/API Routes/r-2/components/LoadMore.tsx
@@ -1,19 +1,29 @@
 'use client';
 
+import { fetchAnime } from '@/app/action';
 import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
+import AnimeCard, { AnimeProp } from './AnimeCard';
 function LoadMore() {
   const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]);
   //console.log(inView); // false
 
   useEffect(() => {
     if (inView) {
-      alert('bombakla');
+      fetchAnime(2).then((res) => {
+        setData([...data, ...res]);
+      });
     }
-  }, [inView]);
+  }, [inView, data]);
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
           {/* Once the ref is reached inView become true*/}
