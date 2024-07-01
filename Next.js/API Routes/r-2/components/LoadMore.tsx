1c83f01c238f947591f3d22d1e478cfe77cce51a
Maksym <tarasovcad@gmail.com>
2024-06-30 19:49:11 -0500

    feat: when screen reached loading component alert happened for r-2

--- Next.js/API Routes/r-2/components/LoadMore.tsx
+++ Next.js/API Routes/r-2/components/LoadMore.tsx
@@ -1,10 +1,22 @@
-import Image from "next/image";
+'use client';
 
+import Image from 'next/image';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 function LoadMore() {
+  const { ref, inView } = useInView();
+  //console.log(inView); // false
+
+  useEffect(() => {
+    if (inView) {
+      alert('bombakla');
+    }
+  }, [inView]);
   return (
     <>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
+          {/* Once the ref is reached inView become true*/}
           <Image
             src="./spinner.svg"
             alt="spinner"
