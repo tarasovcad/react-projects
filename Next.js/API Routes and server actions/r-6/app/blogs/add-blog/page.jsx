c534c3daa4dbf84a982d734887be622d498da5c4
Maksym <tarasovcad@gmail.com>
2024-07-09 18:29:17 -0500

    feat: add github provider for a-3

--- Next.js/API Routes and server actions/r-6/app/blogs/add-blog/page.jsx
+++ Next.js/API Routes and server actions/r-6/app/blogs/add-blog/page.jsx
@@ -18,3 +18,13 @@ export default function AddBlog() {
     </>
   );
 }
+
+import mongoose from 'mongoose';
+import { User } from '../../models/User';
+
+export async function POST(req) {
+  const body = await req.json();
+  mongoose.connect(process.env.MONGO_URL);
+  const createdUser = await User.create(body);
+  return Response.json(createdUser);
+}
