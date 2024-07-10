c51a17d1898c3dcebeb9587b9158ced09b988755
Maksym <tarasovcad@gmail.com>
2024-07-09 23:02:29 -0500

    feat: add protecting page using middleware

---
+++ Next.js/authentication/a-4/middleware.js
@@ -0,0 +1,20 @@
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+
+export default withAuth(
+  function middleware(req) {
+    console.log(req.nextUrl.pathname);
+    console.log(req.nextauth.token.role);
+
+    if (req.nextUrl.pathname.startsWith('/CreateUser') && req.nextauth.token.role != 'Admin') {
+      return NextResponse.rewrite(new URL('/Denied', req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  },
+);
+
+export const config = { matcher: ['/CreateUser'] };
